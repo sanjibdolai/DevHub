0badb7ee72d6a68e920a9151b40eafa37622e26b
Sanjib Dolai <90773927+sanjibdolai@users.noreply.github.com>
2025-06-29 20:50:14 +0530

    update

---
+++ api/json-server.ts
@@ -0,0 +1,16 @@
+import jsonServer from 'json-server';
+import path from 'path';
+import middleware from '../middleware.cjs';
+// Use require for CommonJS middleware
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(process.cwd(), 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(middleware);
+server.use(router);
+
+export default (req, res) => {
+    server(req, res);
+};
