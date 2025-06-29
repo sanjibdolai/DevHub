ffca432353f10d9f37f01a170ca3abe03e334303
Sanjib Dolai <90773927+sanjibdolai@users.noreply.github.com>
2025-06-29 20:24:24 +0530

    remove

--- middleware.js
+++
@@ -1,6 +0,0 @@
-// Add this file to expose the x-total-count header for CORS in json-server
-
-module.exports = (req, res, next) => {
-  res.header('Access-Control-Expose-Headers', 'X-Total-Count');
-  next();
-};
