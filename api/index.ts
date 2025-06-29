dea3308bd55bccb8894362a7a67aceaac93d1253
Sanjib Dolai <90773927+sanjibdolai@users.noreply.github.com>
2025-06-29 21:20:30 +0530

    updated

--- api/index.ts
+++ api/index.ts
@@ -10,6 +10,11 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(middleware);
 server.use(router);
+// Handle API requests
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1'
+}));
+
 
 export default (req, res) => {
     server(req, res);
